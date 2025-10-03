cd5e218d62d98d904b1f104ad3a021de8629d42b
baptistegoncalves <baptiste.goncalves@epitech.digital>
2025-10-03 15:20:30 +0200

    ADD tuto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 "use client";
 import Bento_Doug from "@/components/Bento-grid/Bento-grid";
+import TutorialModal from "@/components/TutorialModal";
 export default function Home() {
-  return <Bento_Doug />;
+  return (
+    <>
+      <TutorialModal />
+      <Bento_Doug />
+    </>
+  );
 }
